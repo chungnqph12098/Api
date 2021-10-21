4d5ba162dd160796d56957cb97845cff244ed48a
Ngo ANh <anhngongoc1803@gmail.com>
2021-10-21 20:43:33 +0700

    capnhamoi13/10

--- DATN/src/main/java/DATN/Class/reports.java
+++ DATN/src/main/java/DATN/Class/reports.java
@@ -1,16 +1,11 @@
 package DATN.Class;
 
+import java.io.Serializable;
 import java.util.Date;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -22,17 +17,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Entity
 @Table(name = "reports")
-public class reports {
+public class Reports implements Serializable{
 	@Id
 	private Integer id;
+	private Integer user_id;
 	private String content;
-	private Boolean status;
+	private Integer status;
 	private Date create_date;
-	@ManyToOne
-	@JoinColumn(name = "User_id")
-	Users users;
 	
-	@JsonIgnore
-	@OneToMany(mappedBy = "reports")
-	List<user_report> user_report;
+	
 }
