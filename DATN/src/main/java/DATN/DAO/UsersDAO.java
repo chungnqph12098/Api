fa5619d2afb71d9c1f41340766079fcc63e557c1
chung200121 <chungnqph12098@fpt.edu.vn>
2021-10-22 22:42:18 +0700

    fix mapping class

--- DATN/src/main/java/DATN/DAO/UsersDAO.java
+++ DATN/src/main/java/DATN/DAO/UsersDAO.java
@@ -1,17 +1,9 @@
 package DATN.DAO;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import DATN.Class.QuestionHS;
 import DATN.Class.Users;
 
 public interface UsersDAO extends JpaRepository<Users,Integer> {
-//	@Query("select u from users u where users=?1")
-//	List<Users> find(Integer id);
 
-//	@Query("select p from users q where p.users=?1")
-//	List<Users> find(QuestionHS questionhs);
 }
--- DATN/src/main/java/DATN/DAO/quetionHSDAO.java
+++ DATN/src/main/java/DATN/DAO/quetionHSDAO.java
@@ -1,18 +1,10 @@
 package DATN.DAO;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import DATN.Class.Posts;
 import DATN.Class.QuestionHS;
-import DATN.Class.Users;
 
 
 public interface quetionHSDAO extends JpaRepository<QuestionHS, Integer> {
-//	@Query("select p from user p where p.posts=?1")	
-//	List<Users> find(Posts posts);
-
 
 }
